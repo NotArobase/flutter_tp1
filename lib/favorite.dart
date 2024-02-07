f65190bcb8dd772376dc7e4bba841a7d336f509a
NotArobase <143614716+NotArobase@users.noreply.github.com>
2024-02-07 11:56:28 +0100

    Favorite + fix doublons

--- lib/favorite.dart
+++ lib/favorite.dart
@@ -1,92 +1,14 @@
 import 'package:flutter/material.dart';
-import 'favorite.dart';
+import 'main.dart';
 
-/// Flutter code sample for [BottomNavigationBar].
-
-void main() => runApp(const MediaApp());
-
-class MediaApp extends StatelessWidget {
-  const MediaApp({super.key});
+class ListFavorite extends StatefulWidget {
+  const ListFavorite({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: BottomNavigationBarExample(),
-    );
-  }
+  ListFavoriteState createState() => ListFavoriteState();
 }
 
-class BottomNavigationBarExample extends StatefulWidget {
-  const BottomNavigationBarExample({super.key});
-
-  @override
-  State<BottomNavigationBarExample> createState() =>
-      _BottomNavigationBarExampleState();
-}
-
-class _BottomNavigationBarExampleState
-    extends State<BottomNavigationBarExample> {
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    ListTileSelectExample(),
-    ListFavorite(),
-    Text(
-      'Index 2: About',
-      style: optionStyle,
-    ),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-            backgroundColor: Colors.red,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star),
-            label: 'Favorites',
-            backgroundColor: Colors.green,
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.info),
-            label: 'About',
-            backgroundColor: Colors.purple,
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-    );
-  }
-}
-
-
-
-class ListTileSelectExample extends StatefulWidget {
-  const ListTileSelectExample({super.key});
-
-  @override
-  ListTileSelectExampleState createState() => ListTileSelectExampleState();
-}
-
-class ListTileSelectExampleState extends State<ListTileSelectExample> {
+class ListFavoriteState extends State<ListFavorite> {
   bool isSelectionMode = false;
   final int listLength = films.length;
   late List<bool> _selected;
@@ -164,15 +86,18 @@ class ListTileSelectExampleState extends State<ListTileSelectExample> {
                   }),
             if (isSelectionMode)
               IconButton(
-                icon: const Icon(Icons.favorite),
+                icon: const Icon(Icons.heart_broken),
                 onPressed: () {
                   // Ajouter les éléments sélectionnés à la liste de favoris
                   for (int i = 0; i < _selected.length; i++) {
-                    if (_selected[i] && !favorites.contains(films[i])) {
-                      favorites.add(films[i]);
+                    if (_selected[i]) {
+                      favorites.remove(films[i]);
                     }
                   }
                   // Sortir du mode de sélection
+                  setState(() {
+                    favorites = [...favorites]; // Cette ligne force la mise à jour de l'état
+                  });
                   setState(() {
                     isSelectionMode = false;
                     // Remettre à zéro la sélection
@@ -249,13 +174,16 @@ class GridBuilderState extends State<GridBuilder> {
           },
           child: GridTile(
             child: Stack(
-              children: [Center(child: 
-                Image.asset(
-                    films.isNotEmpty && index < films.length ? films[index].img : '',
+              children: [
+                if(favorites.isNotEmpty) 
+                  Center(
+                  child: Image.asset(
+                    favorites[index].img,
                     width: 120,
                     height: 160,
-                  ),),
-                if (widget.selectedList[index])
+                  ),
+                ),
+                if (widget.selectedList[index] && favorites.isNotEmpty)
                   Center(
                       child: Checkbox(
                         onChanged: (bool? x) => _toggle(index),
@@ -300,7 +228,11 @@ class _ListBuilderState extends State<ListBuilder> {
   Widget build(BuildContext context) {
     return ListView.builder(
         itemCount: widget.selectedList.length,
-        itemBuilder: (_, int index) {
+        itemBuilder: (_, int index)  {
+        if (favorites.isEmpty) {
+        // Si la liste de favoris est vide, ne rien retourner
+        return const SizedBox.shrink();
+      } else {
           return ListTile(
               onTap: () => _toggle(index),
               onLongPress: () {
@@ -317,52 +249,7 @@ class _ListBuilderState extends State<ListBuilder> {
                       onChanged: (bool? x) => _toggle(index),
                     )
                   : const SizedBox.shrink(),
-              title: Text(films[index].nom));
-        });
+              title: Text(index < favorites.length ? favorites[index].nom : '',));
+  }});
   }
 }
-
-class Film {
-  final String nom;
-  final String img;
-  final List<String> acteurs;
-  final int dateDeSortie;
-  final String synopsis;
-
-  Film({
-    required this.nom,
-    required this.img,
-    required this.acteurs,
-    required this.dateDeSortie,
-    required this.synopsis,
-  });
-}
-
-Film lupin = Film(
-  nom: "Lupin",
-  img: "assets/img/lupin.jpg",
-  acteurs: ["Omar Si", "Mario"],
-  dateDeSortie: 2023,
-  synopsis: "bonsoir",
-);
-
-Film inception = Film(
-  nom: "Inception",
-  img: "assets/img/inception.jpg",
-  acteurs: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"],
-  dateDeSortie: 2010,
-  synopsis: "Un voleur expérimenté vole des secrets précieux dans les rêves de ses cibles.",
-);
-
-Film theDarkKnight = Film(
-  nom: "The Dark Knight",
-  img: "assets/img/the-dark-knight.jpg",
-  acteurs: ["Christian Bale", "Heath Ledger"],
-  dateDeSortie: 2008,
-  synopsis: "Batman s'associe à Gordon et Harvey Dent pour éliminer le crime organisé de Gotham City. Un criminel brillant connu sous le nom de Joker sème le chaos et la terreur.",
-);
-
-// Création de la liste de films
-List<Film> films = [lupin, inception, theDarkKnight];
-List<Film> favorites = [];
-
