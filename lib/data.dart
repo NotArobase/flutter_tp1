57d1b8697b640ee6534beb6f2ef369f0e71a7bb6
= <=>
2024-02-12 19:23:10 +0100

    ajout medias

--- lib/data.dart
+++ lib/data.dart
@@ -5,34 +5,33 @@ import 'bd.dart';
 import 'livre.dart';
 
 BD asterix = BD(
-      nom: "Astérix",
-      img: "assets/img/asterix-and-obelix-i167755.jpg",
-      auteur: "René Goscinny",
-      dateDeSortie: 1959,
-      synopsis: "Les aventures d'un Gaulois et de son ami Obélix",
-    );
-BD tintin = 
-    BD(
-      nom: "Tintin en Amérique",
-      img: "assets/img/tintin-en-amerique-fr.png",
-      auteur: "Hergé",
-      dateDeSortie: 1929,
-      synopsis: "Les aventures d'un jeune reporter et de son chien Milou",
-    );
+  nom: "Astérix",
+  img: "assets/img/asterix-and-obelix-i167755.jpg",
+  auteur: "René Goscinny",
+  dateDeSortie: 1959,
+  synopsis: "Les aventures d'un Gaulois et de son ami Obélix",
+);
+BD tintin = BD(
+  nom: "Tintin en Amérique",
+  img: "assets/img/tintin-en-amerique-fr.png",
+  auteur: "Hergé",
+  dateDeSortie: 1929,
+  synopsis: "Les aventures d'un jeune reporter et de son chien Milou",
+);
 Livre livre1 = Livre(
-      nom: "Le Seigneur des Anneaux",
-      img: "assets/img/J02275.jpg",
-      auteur: "J.R.R. Tolkien",
-      dateDeSortie: 1954,
-      synopsis: "Une quête pour détruire un anneau maléfique",
-    );
+  nom: "Le Seigneur des Anneaux",
+  img: "assets/img/J02275.jpg",
+  auteur: "J.R.R. Tolkien",
+  dateDeSortie: 1954,
+  synopsis: "Une quête pour détruire un anneau maléfique",
+);
 Livre livre2 = Livre(
-      nom: "Harry Potter",
-      img: "assets/img/libriweb.jpg",
-      auteur: "J.K. Rowling",
-      dateDeSortie: 1997,
-      synopsis: "Les aventures d'un jeune sorcier",
-    );
+  nom: "Harry Potter",
+  img: "assets/img/libriweb.jpg",
+  auteur: "J.K. Rowling",
+  dateDeSortie: 1997,
+  synopsis: "Les aventures d'un jeune sorcier",
+);
 Film lupin = Film(
   nom: "Lupin",
   img: "assets/img/lupin.jpg",
@@ -46,7 +45,8 @@ Film inception = Film(
   img: "assets/img/inception.jpg",
   acteurs: ["Leonardo DiCaprio", "Joseph Gordon-Levitt"],
   dateDeSortie: 2010,
-  synopsis: "Un voleur expérimenté vole des secrets précieux dans les rêves de ses cibles.",
+  synopsis:
+      "Un voleur expérimenté vole des secrets précieux dans les rêves de ses cibles.",
 );
 
 Film theDarkKnight = Film(
@@ -54,25 +54,160 @@ Film theDarkKnight = Film(
   img: "assets/img/the-dark-knight.jpg",
   acteurs: ["Christian Bale", "Heath Ledger"],
   dateDeSortie: 2008,
-  synopsis: "Batman s'associe à Gordon et Harvey Dent pour éliminer le crime organisé de Gotham City. Un criminel brillant connu sous le nom de Joker sème le chaos et la terreur.",
+  synopsis:
+      "Batman s'associe à Gordon et Harvey Dent pour éliminer le crime organisé de Gotham City. Un criminel brillant connu sous le nom de Joker sème le chaos et la terreur.",
 );
 
 Serie serie1 = Serie(
-      nom: "Game of Thrones",
-      img: "assets/img/0717778.jpg-c_310_420_x-f_jpg-q_x-xxyxx.jpg",
-      acteurs: ["Kit Harington", "Emilia Clarke"],
-      episodes: 73,
-      dateDeSortie: 2011,
-      synopsis: "Une lutte de pouvoir dans un monde fantastique",
-    );
-Serie serie2= Serie(
-      nom: "Breaking Bad",
-      img: "assets/img/3956503.jpg-c_310_420_x-f_jpg-q_x-xxyxx.jpg",
-      acteurs: ["Bryan Cranston", "Aaron Paul"],
-      episodes: 62,
-      dateDeSortie: 2008,
-      synopsis: "Un professeur de chimie devient un fabricant de drogue",
-    );
+  nom: "Game of Thrones",
+  img: "assets/img/0717778.jpg-c_310_420_x-f_jpg-q_x-xxyxx.jpg",
+  acteurs: ["Kit Harington", "Emilia Clarke"],
+  episodes: 73,
+  dateDeSortie: 2011,
+  synopsis: "Une lutte de pouvoir dans un monde fantastique",
+);
+Serie serie2 = Serie(
+  nom: "Breaking Bad",
+  img: "assets/img/3956503.jpg-c_310_420_x-f_jpg-q_x-xxyxx.jpg",
+  acteurs: ["Bryan Cranston", "Aaron Paul"],
+  episodes: 62,
+  dateDeSortie: 2008,
+  synopsis: "Un professeur de chimie devient un fabricant de drogue",
+);
 
-List<Media> media = [lupin, inception, theDarkKnight, tintin, asterix, livre1, livre2, serie1, serie2];
+BD maus = BD(
+  nom: "Maus",
+  img: "assets/img/maus.jpg",
+  auteur: "Art Spiegelman",
+  dateDeSortie: 1991,
+  synopsis:
+      "Une bande dessinée qui raconte l'histoire de l'Holocauste avec des animaux anthropomorphes.",
+);
+
+Serie walkingDead = Serie(
+  nom: "The Walking Dead",
+  img: "assets/img/walking_dead.jpg",
+  acteurs: ["Andrew Lincoln", "Norman Reedus"],
+  episodes: 177,
+  dateDeSortie: 2010,
+  synopsis:
+      "Des survivants dans un monde post-apocalyptique infesté de zombies.",
+);
+
+Livre livre3 = Livre(
+  nom: "1984",
+  img: "assets/img/1984.jpg",
+  auteur: "George Orwell",
+  dateDeSortie: 1949,
+  synopsis:
+      "Un roman dystopique qui explore les thèmes de la surveillance, de la manipulation et de la répression gouvernementale.",
+);
+
+Livre livre4 = Livre(
+  nom: "Le Petit Prince",
+  img: "assets/img/petit_prince.jpg",
+  auteur: "Antoine de Saint-Exupéry",
+  dateDeSortie: 1943,
+  synopsis:
+      "Un conte philosophique sur l'amitié, l'amour et la solitude, mettant en scène un jeune prince venu d'une autre planète.",
+);
+
+Film pulpFiction = Film(
+  nom: "Pulp Fiction",
+  img: "assets/img/pulp_fiction.jpg",
+  acteurs: ["John Travolta", "Uma Thurman"],
+  dateDeSortie: 1994,
+  synopsis:
+      "Une histoire non linéaire entrelaçant plusieurs récits de crime à Los Angeles.",
+);
+
+Film theGodfather = Film(
+  nom: "The Godfather",
+  img: "assets/img/parrain.jpg",
+  acteurs: ["Marlon Brando", "Al Pacino"],
+  dateDeSortie: 1972,
+  synopsis:
+      "Le patriarche d'une famille criminelle transfère le contrôle de son empire clandestin à son fils corrompu.",
+);
+
+BD gastonLagaffe = BD(
+  nom: "Gaston Lagaffe",
+  img: "assets/img/gaston.jpg",
+  auteur: "André Franquin",
+  dateDeSortie: 1957,
+  synopsis:
+      "Les gaffes et les inventions farfelues d'un employé de bureau paresseux et créatif.",
+);
+
+BD duckTales = BD(
+  nom: "DuckTales",
+  img: "assets/img/picsou.jpg",
+  auteur: "Carl Barks",
+  dateDeSortie: 1947,
+  synopsis:
+      "Les aventures de Picsou et de ses neveux dans leur quête de trésors cachés.",
+);
+
+Film oss117 = Film(
+  nom: "OSS 117",
+  img: "assets/img/oss117.jpg",
+  acteurs: ["Jean Dujardin", "Bérénice Bejo"],
+  dateDeSortie: 2006,
+  synopsis:
+      "Les aventures comiques d'un agent secret français arrogant et maladroit pendant les années 1950 et 1960.",
+);
+
+Serie malcolm = Serie(
+  nom: "Malcolm",
+  img: "assets/img/malcolm.jpg",
+  acteurs: ["Frankie Muniz", "Bryan Cranston"],
+  episodes: 151,
+  dateDeSortie: 2000,
+  synopsis:
+      "Un enfant surdoué navigue à travers les hauts et les bas de l'adolescence dans une famille excentrique.",
+);
+
+Serie strangerThings = Serie(
+  nom: "Stranger Things",
+  img: "assets/img/stranger_things.jpg",
+  acteurs: ["Millie Bobby Brown", "Finn Wolfhard"],
+  episodes: 34,
+  dateDeSortie: 2016,
+  synopsis:
+      "Des événements étranges se produisent dans une petite ville de l'Indiana alors qu'un groupe d'enfants cherche leur ami disparu.",
+);
+
+Serie baki = Serie(
+  nom: "Baki",
+  img: "assets/img/baki.jpg",
+  acteurs: ["Nobunaga Shimazaki", "Takuya Eguchi"],
+  episodes: 101,
+  dateDeSortie: 2018,
+  synopsis:
+      "Le protagoniste, Baki Hanma, s'entraîne avec une intensité intense pour devenir assez fort pour surpasser son père, Yujiro Hanma, le combattant le plus fort du monde.",
+);
+
+List<Media> media = [
+  lupin,
+  inception,
+  theDarkKnight,
+  tintin,
+  asterix,
+  livre1,
+  livre2,
+  serie1,
+  serie2,
+  maus,
+  walkingDead,
+  livre3,
+  livre4,
+  pulpFiction,
+  theGodfather,
+  gastonLagaffe,
+  duckTales,
+  oss117,
+  malcolm,
+  strangerThings,
+  baki
+];
 List<Media> favorites = [lupin];
